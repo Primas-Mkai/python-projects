3a0cde35f43c65301f1e839579e913bc48ba368b
Primas Mkai <mkaiprimas1@gmail.com>
2026-03-27 10:45:55 +0300

    first commit

---
+++ 1-Beginner-projects/1-Simple-calculator.py
@@ -0,0 +1,17 @@
+
+def add(num1, num2):
+    return num1 + num2
+
+def subract(num1, num2):
+    return num1 - num2
+
+def multiply(num1, num2):
+    return num1 * num2
+
+def divide(num1, num2):
+    return num1 / num2
+
+print(add(4, 2))
+print(subract(4, 2))
+print(multiply(4, 2))
+print(divide(4, 2))
