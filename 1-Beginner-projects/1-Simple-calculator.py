b3d4cabcc6611ab458dffb78a62487599d9eeda9
Primas Mkai <mkaiprimas1@gmail.com>
2026-03-31 13:48:13 +0300

    final commit

--- 1-Beginner-projects/1-Simple-calculator.py
+++ 1-Beginner-projects/1-Simple-calculator.py
@@ -1,8 +1,7 @@
-
 def add(num1, num2):
     return num1 + num2
 
-def subract(num1, num2):
+def sub(num1, num2):
     return num1 - num2
 
 def multiply(num1, num2):
@@ -11,7 +10,24 @@ def multiply(num1, num2):
 def divide(num1, num2):
     return num1 / num2
 
-print(add(4, 2))
-print(subract(4, 2))
-print(multiply(4, 2))
-print(divide(4, 2))
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print("Select operation \n"
+      "1. +\n"
+      "2. -\n"
+      "3. *\n" 
+      "4. /\n")
+
+sel = int(input("select(1 - 4): "))
+
+if sel == 1:
+    print(add(num1, num2))
+elif sel == 2:
+    print(sub(num1, num2))
+elif sel == 3:
+    print(multiply(num1, num2))
+elif sel == 4:
+    print(divide(num1, num2))
+else:
+    print("Inalid input")
